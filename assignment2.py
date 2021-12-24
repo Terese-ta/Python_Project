dd46d2ae07dba39bbdacff0ed46d4ed67bf67501
Terese <terese.akuma@yahoo.ca>
2021-12-24 18:24:00 -0500

    added assignment2.py

---
+++ assignment2.py
@@ -0,0 +1,84 @@
+#PYTHON ASSIGNMENT 2
+#first things first, since y'all did not get it the last time. Copy Everything on #this page into a #file and name it with a .py extension
+#This ultimately means that whatever you send me should be executable
+#Identify the type of each of the following variables and write it as a #comment next to the variable.
+type(299)   # int
+type(90.0)   # float
+type("145")  #str
+
+type("\u0CA0_\u0CA0")  #str
+type("True")   # str
+type(True)   #bool
+type(len('Sample'))   #int
+type(100**30)  #int
+type(1 >= 1)   #bool
+type(30%7)  #int
+type(30/7)   #float
+b = 1
+type(b + 7)  #int
+type(128 << 1)  #int
+type(bin(255))   #string
+
+[k, l, m, n ]) 
+ l = 6
+  m = 7
+  n = 8
+  type([k, l, m, n])  #list
+  #What value is at the end of the varialbe my_var at the end of these assignments. Add a comparison after the last statement
+  #in the form of my_val ==
+  my_var = 99
+  my_var += 11
+  my_var = str(my_var)
+  my_var *= 2
+  my_var = len(my_var)
+  my_var *= 4
+  my_var = 99
+  if my_var == 99:
+            print("my_var is  ")
+
+
+            #Change the loop below so that it prints the numbers from 1 to 10
+            for i in range(9)
+            print(i)
+            loop = list(range(10))
+            for i in loop:
+                     if i == 9:
+                                      print(i, "is equal to", range)
+                                               else:
+                                                                print(i)
+                                                                #Save a copy of your favorite snack as a variable. Using that variable, print your favorite snack 100 times
+                                                                snack = "chips "
+                                                                 print((snack + " ") * 100)
+
+                                                                 #Make a list of your grocery items with prices 9.42, 5.67, 3.25, 13.40, 7.50 and store it as a variable. Using a builtin function, #find the cheapest and the most expensive items on your shopping list
+                                                                 my_groceries = {"cokkies":9.42, "apple":5.67, "orange":3.25, "banana":13.40, "eggnog":7.50}
+                                                                 print(my_groceries)
+                                                                 print(max(my_groceries))
+                                                                 print(min(my_groceries))
+
+                                                                 #Import random. run dir(random.randint). Use Randint to randomly print between 0 and 100 copies of your favorite snack
+                                                                 snack = "chips"
+                                                                 import random
+                                                                 random.choice(["chips"])
+                                                                 import random
+                                                                 snack = ["chips"]
+                                                                 random.choice(snack)
+                                                                 from random import shuffle
+                                                                 x = [[i] for i in range(0,100)]
+                                                                 shuffle(x)
+                                                                 print(x)
+
+                                                                 #Run dir(random). Find a function in random that you can use to print a random item from your grocery list. Remember you can use #help to find out what functions do.
+                                                                 #Write code to randomly select items from your groceries list, round them to the nearest integer and print the result
+                                                                 #Previously,we used the * operator to print a hundred copies of your favorite snack. This time use a while loop to print 50 copies of your favorite snack.
+                                                                 #Mix and Match! Write a while loop that uses the * operator to print multiple copies of your favorite snack per line. Print out 10 lines where the first line has one copy of your favorite snack and the last line hast 10.
+                                                                 #Challenge: Your neighborhood grocery store is having a weird promotion called "Win free change". A random item is picked from your #grocery list and you pay 10 dollars. If the item is less than 10 dollars, you get the item and the balance from the items price. If #the item is more than 10 dollars, you get the item and the difference in the price as change. Write the code to randomly pick a #price from your price list and print out the amount the cashier
+                                                                 Python For DevOps - Saturday/Sunday
+                                                                 When   Sun Dec 19, 2021 7am – 9am Eastern Time - New York
+                                                                 Where  https://e-panzer.webex.com/meet/wtakang (map)
+
+                                                                 Who    (Guest list has been hidden at organizer's request)
+
+                                                                 Ls
+
+
