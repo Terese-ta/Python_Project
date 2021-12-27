1e5e9cfb8a987072b60b7bf0748a529ed520e5ed
Terese <terese.akuma@yahoo.ca>
2021-12-27 14:32:05 -0500

    modified my_dalies.py

--- my_dailies.py
+++ my_dailies.py
@@ -5,39 +5,38 @@ print("Good morning folks")
 #How do you insert comments in python code
 
 class holiday_destination():
-              #The following represents the attributes of my "class", "holiday_destination"
-                        country = "Bali"
-                                  when = "summer, 2022"
-                                            reason = "reward"
-                                                      cost = 500
-                                                                duration = "one week"
-                                                                          number_people = 2
-                                                                                    #I will now define below, the functionality of my class
-                                                                                              def quality(self):
-                                                                                                                print("must be exquisite", "by the lake", "there is room for a third guest", "weath
-                                                                                                                      er must be great, else, notify guests", "must be within clients' budget", "must cater to in
-                                                                                                                            dividualized needs of guests")
+#The following represents the attributes of my "class", "holiday_destination"
+country = "Bali"
+when = "summer, 2022"
+reason = "reward"
+cost = 500
+duration = "one week"
+number_people = 2
+#I will now define below, the functionality of my class
+def quality(self):
+print("must be exquisite", "by the lake", "there is room for a third guest", "weather must be great, else, notify guests", "must be within clients' budget", "must cater to in dividualized needs of guests")
 
-                                                                                                                Terez_destination.quality
+Terez_destination.quality
 
-                                                                                                                 
-                                                                                                                 #How do you create a variable with the numeric value  5
+#How do you create a variable with the numeric value  5
 
-                                                                                                                 number = 5
+number = 5
+#What's the correct extension for python files
+#<file name>.py
+#eg: 
+example.py
 
-                                                                                                                 #What's the correct extension for python files
-                                                                                                                 <file name>.py
-                                                                                                                 eg: 
-                                                                                                                     example.py
-
-                                                                                                                      
-                                                                                                                      #How do you create a variable with the floating variable 8.6cd usr
+#How do you create a variable with the floating variable 8.6cd usr
 
 
-                                                                                                                      value = 8.6
-                                                                                                                      value1 = cd
-                                                                                                                      type("value""value1")  #str
-                                                                                                                      value = "8.6cd"
+value = 8.6
+value1 = cd 
+type("value""value1")  #str
+value = "8.6cd"
+
+
+
+
 
 
 
