f6075d0bb05fab942e23068522b4830c50814c13
Venza-Tech <83881108+Terese-ta@users.noreply.github.com>
2021-12-27 16:28:41 -0500

    Update my_dailies.py

--- my_dailies.py
+++ my_dailies.py
@@ -4,34 +4,35 @@ print("Good morning folks")
 
 #How do you insert comments in python code
 class holiday_destination():
-#The following represents the attributes of my "class", "holiday_destination"
-country = "Bali"
-when = "summer, 2022"
-reason = "reward"
-cost = 500
-duration = "one week"
-number_people = 2
-specs = ["must be exquisite", "by the lake", "there is room for a third guest", "weather must be great, else, notify guests", "must be within clients' budget", "must cater to individualized needs of guests"]
-#I will now define below, the functionality of my class
-def quality(self):
-for i in self.specs:
+    #The following represents the attributes of my "class", "holiday_destination"
+    country = "Bali"
+    when = "summer, 2022"
+    reason = "reward"
+    cost = 500
+    duration = "one week"
+    number_people = 2
+    specs = ["must be exquisite", "by the lake", "there is room for a third guest", "weather must be great, else, notify guests", "must be within clients' budget", 
+         "must cater to individualized needs of guests"]
+    #I will now define below, the functionality of my class
+    def quality(self):
+    for i in self.specs:
     print(i)
-                                                                                                                                       Terez_destination = holiday_destination()
+Terez_destination = holiday_destination()
 
-                                                                                                                                    
-                                                                                                                                       Terez_destination.quality()
+Terez_destination.quality()
+
+
 #How do you create a variable with the numeric value  5
-
 number = 5
+
 #What's the correct extension for python files
 #<file name>.py
 #eg: 
 example.py
 
 #How do you create a variable with the floating variable 8.6cd usr
-
-
 value = 8.6
+type(value)  #float
  
 
 
