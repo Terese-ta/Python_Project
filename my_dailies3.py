767f1f4ebb5e2ab25ffeed5f74dc407a7a9787be
Terese <terese.akuma@yahoo.ca>
2021-12-30 15:06:34 -0500

    added assignment3.py, my_dailies3.py, my_dailies4.py

---
+++ my_dailies3.py
@@ -0,0 +1,58 @@
+#1. What is the output of the following range() function
+for num in range(2,-5,-1):
+        print(num, end=", ")
+        start = 2 
+        stop = -5, 
+        step = -1
+        reverse index range ==> 2, 1, 0, -1, -2, -3,-4, -5
+
+        start, end = 2, -5
+        for num in range(start, end + -1):
+                       if number < 0:
+                                          print(num, end = " ")
+
+
+                                          #2. Given the nested if-else below, what will be the value x when the code executed successfully
+                                          x = 0
+                                          a = 5
+                                          b = 5
+                                          if a > 0:
+                                                  if b < 0:
+                                                              x = x + 5
+                                                                  elif a > 5:
+                                                                              x = x + 4
+                                                                                  else:
+                                                                                              x = x + 3
+                                                                                          else:
+                                                                                                  x = x + 2
+                                                                                                  print(x)
+
+
+                                                                                                  #3. What is the output of the following nested loop?
+                                                                                                  for num in range(10, 14):
+                                                                                                         for i in range(2, num):
+                                                                                                                    if num%i == 1:
+                                                                                                                                  print(num)
+                                                                                                                                            break
+
+
+                                                                                                                                        #4. What is the output of the following if statement
+                                                                                                                                        a, b = 12, 5
+                                                                                                                                        if a + b:
+                                                                                                                                                print('True')
+                                                                                                                                            else:
+                                                                                                                                                  print('False')
+                                                                                                                                                  True
+
+
+                                                                                                                                                  #5. What is the value of the var after the for loop completes its execution
+                                                                                                                                                  var = 10
+                                                                                                                                                  for i in range(10):
+                                                                                                                                                          for j in range(2, 10, 1):
+                                                                                                                                                                      if var % 2 == 0:
+                                                                                                                                                                                      continue
+                                                                                                                                                                                              var += 1
+                                                                                                                                                                                                  var+=1
+                                                                                                                                                                                              else:
+                                                                                                                                                                                                      var+=1
+                                                                                                                                                                                                      print(var)
---
+++ my_dailies4.py
@@ -0,0 +1,28 @@
+
+Question of the day: Thursday December 29th 2021
+#1. What is the output of print(2 ** 3 ** 2)
+ var = 10
+
+ print(2 ** 3 ** 2)
+ 512
+
+ #2. What is the output of print(10 - 4 * 2)
+
+ print(10 - 4 * 2)
+ 2
+
+ #3. What is the value of the following Python Expression
+ print(36 / 4)
+ 9.0
+
+ #4. What is the output of the following Python code
+ x = 10
+ y = 50
+ if x ** 2 > 100 and y < 100:
+         print(x, y)
+
+#5. What is the output of the following code
+x = 100
+y = 50
+print(x and y)
+50
