4c897062be4b9088f078c374fa918103b72edd46
Terese <terese.akuma@yahoo.ca>
2022-01-14 13:17:00 -0500

    added my_dailies6.py

---
+++ my_dailies6.py
@@ -0,0 +1,39 @@
+Question of the day for January 11th 2022
+
+
+#Create a function func() which takes a variale number of arguments and prints out those variables.
+#So for example if func(23, 25, 25)
+#it should print 23, 25, and 25,
+#if func(20, 21)
+#it should print 20, 21
+
+def func():
+         #function prints variables and their arguments
+         list = [20, 30, 40, 50]
+         print(list)
+
+
+#Write a program to create function calculation() such that it can accept two variables and calculate addition and subtraction.
+#Also, it must return both addition and subtraction in a single return call.
+
+def my_function():
+       print([3 + 4], [4-3])
+
+
+
+#Write a program to create a function show_employee() using the following conditions.
+#It should accept the employee’s name and salary and display both.
+
+def show_salary():
+         #This function shows employees and their salaries
+         employees = {"Richard":100, "Okere":100}
+         print(employees)
+
+
+#If the salary is missing in the function call then assign default value 9000 to salary
+def show_salary():
+         #This function shows employees and their salaries
+         employees = {"Richard":100, "Okere":100}
+         print(employees)
+
+show_salary("Okere")
