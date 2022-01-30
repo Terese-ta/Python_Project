e498c1975103e6bab2e1762e3e13c36a019eb9ca
Terese <terese.akuma@yahoo.ca>
2022-01-30 11:53:29 -0500

    added my_dailies8.py

---
+++ my_dailies8.py
@@ -0,0 +1,57 @@
+Question of the day for Monday January 17th 2022
+
+1. Which of the following is an operator and which is a value
+Operators:
+*
+-
+/
++
+Others: / // % **
+
+Values:
+'Hello'
+-88.88
+5
+
+2. Which of these is a variable and which is a string
+spam  ==> variable
+'spam' ==> string  
+
+3. Name three data types
+   a)string
+   b)integer
+   c)list
+   
+4. what is an expression made of. What do all expressions do?
+   It is a combination of operators and operands that are interpreted to form another value.
+   Examples include, arithmentic expressions, constant expressions, integral expressions,
+   floating expressions, relational expressions, logical expressions, bitwise expressions, 
+   combinational expressions, regular expressions.
+   In any programming language, an expression is evaluated as per the precedence of its operators.
+   Thereore, if there is more than one operator in an expression, theire precedence decides which 
+   operation will be perormed first.
+   It is a combination of data objects, forming expressions.
+
+   Example of arithmetic expressions:
+   
+	# Arithmetic Expressions
+	x = 40
+	y = 12
+  
+	add = x + y
+	sub = x - y
+	pro = x * y
+	div = x / y
+        
+	print(add)
+	print(sub)
+	print(pro)
+	print(div
+
+   
+5. What does the variable bacon contain after running the the code below
+bacon = 20
+bacon + 1
+
+It adds one to the output of becon, and so on. Therefore, bacon + 1 gives an output o 21.
+
